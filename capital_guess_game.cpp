d3999a5057213c200025dc94ee52d1bf166b9ba7
krishnarathi-27 <krishna2707@gmail.com>
2021-10-17 23:15:20 +0530

    First Commit

---
+++ capital_guess_game.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+
+    string array[28] = {"AndhraPradesh", "ArunachalPradesh", "Assam", "Bihar", "Chhatisgarh", "Goa", "Gujarat", "Haryana", "HimachalPradesh",
+                        "Jharkhand", "Karnataka", "Kerala", "MadhyaPradesh", "Maharashtra", "Manipur", "Meghalya", "Mizoram", "Nagaland",
+                        "Odisha", "Punjab", "Rajasthan", "Sikkim", "TamilNadu", "Telangana", "Tripura", "UttarPradesh", "Uttarkhand",
+                        "WestBengal"};
+    string check[28] = {"Amravati", "Itanagar", "Dispur", "Patna", "Raipur", "Panaji", "Gandhinagar", "Chandigarh", "Shimla", "Ranchi",
+                        "Bengalaru", "Thiruvananthapuram", "Bhopal", "Mumbai", "Impal", "Shillong", "Aizwal", "Kohima", "Bhubneshwar",
+                        "Chandigarh", "Jaipur", "Gangtok", "Chennai", "Hyderabad", "Agartala", "Lucknow", "Dehradun", "Kolkata"};
+    string input;
+    char choice_1;
+    static int score=0;
+
+    cout << "A state will be displayed , you have to guess the capital of that state correct " << endl;
+  do{
+
+    int Randchoice = rand() % 28;
+
+    cout << "State is : " ;
+    cout << array[Randchoice] << endl;
+
+    fflush(stdin);
+    cout << "Enter the capital of the state (Enter the first letter capital !!) " << endl;
+    cin >> input;
+
+    if (input == check[Randchoice])
+    {
+       cout << "Your answer is correct " << endl;
+       score++;
+       cout << "Score is : " << score << endl;
+
+    }
+    
+    else
+    {
+        cout << "Your answer is wrong " << endl;
+        score--;
+        cout << "Score is : " << score << endl;
+
+    }
+
+    cout << "Do you want to continue " << endl;
+    cin >> choice_1;
+
+  }while(choice_1=='y' || choice_1=='Y');
+
+    return 0;
+}
